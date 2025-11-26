65d6f5971dff5035bafbe6fed226ef6019fd4809
CeciliaZ030 <zhangyixin319@gmail.com>
2025-11-26 21:53:34 +0800

    really working

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -2,7 +2,7 @@ import { AomiFrame } from "@/components/aomi-frame";
 
 export default function Home() {
   return (
-    <main className="bg-white flex min-h-screen items-center justify-center p-6">
+    <main className="flex min-h-screen items-center justify-center bg-black p-6"> 
       <AomiFrame
         height="720px"
         width="1200px"
