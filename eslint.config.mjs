ddeed004b2dc01b9a23cabdb7e4f7f0f3eac8169
CeciliaZ030 <zhangyixin319@gmail.com>
2025-11-28 18:23:14 +0800

    added cli & lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,32 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const [nextConfig] = compat.extends("next/core-web-vitals", "next/typescript");
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ...nextConfig,
+    ignores: [
+      "**/.next/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/node_modules/**",
+      "**/.turbo/**",
+    ],
+    languageOptions: {
+      ...(nextConfig?.languageOptions ?? {}),
+      parser: tsParser,
+    },
+    plugins: {
+      ...(nextConfig?.plugins ?? {}),
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...(nextConfig?.rules ?? {}),
+      // We lint a library + an example app; there is no root pages/app dir.
+      "next/no-html-link-for-pages": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
