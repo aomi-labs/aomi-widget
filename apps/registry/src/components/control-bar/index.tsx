aa8ee1a190b9576f2775c3b602f5d5db86af22e7
CeciliaZ030 <zhangyixin319@gmail.com>
2026-02-24 16:23:17 -0700

    sync component updates

--- apps/registry/src/components/control-bar/index.tsx
+++ apps/registry/src/components/control-bar/index.tsx
@@ -39,7 +39,7 @@ export const ControlBar: FC<ControlBarProps> = ({
   hideNamespace = false,
   hideApiKey = false,
   hideWallet = true,
-  hideNetwork = true,
+  hideNetwork = false,
 }) => {
   return (
     <div className={cn("flex items-center gap-2", className)}>
